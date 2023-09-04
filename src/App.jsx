f11bb000fcd922c22f747d3381c1c5bac7e07163
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-04 15:21:40 -0300

    add input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-
-import './App.css'
-
+import "./App.css"
+import useForm from "./hooks/useForm"
+import Input from "./components/Input"
 function App() {
-  
+  const [form, handleChange] = useForm({ name: "" })
+
+  console.log(form)
+  return (
+    <form action="">
+      <Input
+        label="nombre"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <Input
+        label="apellido"
+        name="lastname"
+        value={form.lastname}
+        onChange={handleChange}
+      />
+    </form>
+  )
+}
 
 export default App
